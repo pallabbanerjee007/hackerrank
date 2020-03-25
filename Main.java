e49320a49efd603ee4fa38a472e3866271a41882
Git Server Admin <admin@git-server>
2020-03-25 10:17:54 +0000

    Add initial repository

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+ 
+  public static int levelCount(String word){
+    
+    /*write your code here and return appropriate value*/
+      
+      return 0;
+  }
+  
+  
+  public static void main(String[] args)
+  {
+        
+    System.out.println(levelCount(args[0]));
+  }
+}
+
+
